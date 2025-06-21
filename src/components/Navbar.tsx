a6d587a78e96ecc88e9d42d0f2ec63e21d349352
Md Mehedi Hasan <mehedihassangd@gmail.com>
2025-06-21 21:21:59 +0600

    added some section to art of music

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+
+const Navbar = ({ className }: { className?: string }) => {
+    const [active, setActive] = useState<string | null>(null);
+
+  return (
+    <div
+     className={cn("fixed top-10  h-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+            <MenuItem setActive={setActive} active={active} item="Home"> 
+            </MenuItem>
+        </Link>
+
+         <MenuItem setActive={setActive} active={active} item="Our service"> 
+              <div className="flex flex-col space-y-6">
+                <HoveredLink hrfe="/courses">All courses</HoveredLink>
+              <HoveredLink hrfe="/courses">Basic Music Theory</HoveredLink>
+              <HoveredLink hrfe="/courses">Advanced Composition</HoveredLink>
+              <HoveredLink hrfe="/courses">Songwriting</HoveredLink>
+              <HoveredLink hrfe="/courses">Music Production</HoveredLink>
+              </div>
+            </MenuItem>
+
+            <Link href={"/contact"}>
+            <MenuItem setActive={setActive} active={active} item="Contact Us"> 
+              
+            </MenuItem>
+            </Link>
+      </Menu>
+      
+      </div>
+  )
+}
+
+export default Navbar
